1a98bb6c8912cb59d4136072ae6c94fff5b58f2c
Dedito Cantila Jr <dedito.cantila@gmail.com>
2022-02-14 19:39:50 +0800

    fix: use heroku env port

--- index.js
+++ index.js
@@ -14,8 +14,8 @@
 
 const http = require('http');
 
-const hostname = '127.0.0.1';
-const port = 8081;
+const hostname = '0.0.0.0';
+const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
