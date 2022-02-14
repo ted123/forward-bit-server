0323f57bfae8433e4d8be0b1e3739279063ef866
Dedito Cantila Jr <dedito.cantila@gmail.com>
2022-02-14 19:27:41 +0800

    test: setup server in heroku

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
-const { spawnSync} = require('child_process');
-const installBvm = spawnSync('npm', ['i', '-g', '@teambit/bvm']);
-const installBit = spawnSync('bvm', ['install']);
-const start = spawnSync('bit', ['start']);
-console.error('error bvm', installBvm.error);
-console.log('stdout bvm', installBvm.stdout);
-console.error('stderr bvm', installBvm.stderr);
-console.error('error bit', installBit.error);
-console.log('stdout bit', installBit.stdout);
-console.error('stderr bit', installBit.stderr);
-console.error('error start', start.error);
-console.log('stdout start', start.stdout);
-console.error('stderr start', start.stderr);
+// const { spawnSync} = require('child_process');
+// const installBvm = spawnSync('npm', ['i', '-g', '@teambit/bvm']);
+// const installBit = spawnSync('bvm', ['install']);
+// const start = spawnSync('bit', ['start']);
+// console.error('error bvm', installBvm.error);
+// console.log('stdout bvm', installBvm.stdout);
+// console.error('stderr bvm', installBvm.stderr);
+// console.error('error bit', installBit.error);
+// console.log('stdout bit', installBit.stdout);
+// console.error('stderr bit', installBit.stderr);
+// console.error('error start', start.error);
+// console.log('stdout start', start.stdout);
+// console.error('stderr start', start.stderr);
 
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3100;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
